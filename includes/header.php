6a8cc9decd6d048595d69a51653e70e775ad8c6d
Denise Polly <dpolly@g.harvard.edu>
2014-09-17 00:02:43 -0500

    Initial Draft

---
+++ includes/header.php
@@ -0,0 +1,2 @@
+	<h2 id="headerinfo"> Denise Polly </h2>
+	<h3>Dynamic Web Applications Portfolio</h3>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<?php include("includes/htmlhead.php"); ?>
+		<title>Denise M Polly</title>
+		<script> $(function() { $( "#accordion" ).accordion(); }); </script>		
+	</head>
+	<body>
+		<header> 
+		<?php include("includes/header.php"); ?>
+		</header> 
+		<p id="img"><img alt="Fishing Rainy Lake" src="images/fishing.png"/></p>
+		<h2 class="section"> Project Listing</h2>
+			<ul class="text">
+				<li><a href="Project2/index.php" title="Project 2"> Project 2 </a></li>
+				<li><a href="Project3/index.php" title="Project 2"> Project 3 </a></li>
+				<li><a href="Project4/index.php" title="Project 2"> Project 4 </a></li>
+			</ul>
+		<h2 class="section"> Questions and Answers</h2>
+			<div class="text"> 
+				<p>Question: </br> akdjladkjfaldkjfaldkfjaldkjf;alkdfj;aldkjf;aldkjf;ladkfj;lakdjfla;dkjf;aldkjf</p>
+				<p>Answer: </br>sdfadlskjaldkfja;dkja;ldkfjaldksdfad sfadsfadfadfadfadfkadf;adkjfa;lkdjf;alkdsjf;alkdsjf;alkdsf j;lakdsjf;aldskfj;aldskfj;lasdkjfa;lkdsfja;lkdsfja;ldskfj;alsdkjf;lasdkfj;aldskfja;lsdkfja;ldskjfjf;alskdjf </p>
+				<p>Question: </br> akdjladkjfaldkjfaldkfjaldkjf;alkdfj;aldkjf;aldkjf;ladkfj;lakdjfla;dkjf;aldkjf</p>
+				<p>Answer: </br>sdfadlskjaldkfja;dkja;ldkfjaldksdfad sfadsfadfadfadfadfkadf;adkjfa;lkdjf;alkdsjf;alkdsjf;alkdsf j;lakdsjf;aldskfj;aldskfj;lasdkjfa;lkdsfja;lkdsfja;ldskfj;alsdkjf;lasdkfj;aldskfja;lsdkfja;ldskjfjf;alskdjf </p>
+			</div>
+		<footer>
+			<?php include("includes/footer.php"); ?> 
+		</footer>
+	</body>
+</html>
