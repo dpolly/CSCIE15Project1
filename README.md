a959c98dadf198e87967bd9490058436710712c7
Denise Polly <dpolly@g.harvard.edu>
2014-09-18 00:20:18 -0500

    Added Questions and GitHub Links

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-•Live URL of the project.
-•Description of the project (2-3+ sentences).
-•Demo information: If you attend your section to do an in-person demo, make a note of this. If you opt to do the Jing screencast demo, include the link here.
-•Any details the instructor or TA needs to know, for example, test credentials.
-•A list of any plugins, libraries, packages or outside code used in the project. See Student Responsibilities for more details on avoiding code plagiarism.
+Live Site: http://p1.dpolly.me
+Project Description: Creation of portfolio page for CSCIE 15 Dynamic Web Applications
+Demo: No Demo needed for project one. 
+Notes or Comments: Having issues with picture displaying on public site, shows locally. 
+References: No references for this project. 
--- index.php
+++ index.php
@@ -11,17 +11,32 @@
 		</header> 
 		<div id="picture"> <img class="img" src="images/fishing.png" alt="Fishing at Rainy Lake" width="258" height="387"></div>
 		<h2 class="section"> Project Listing</h2>
-			<ul class="text">
-				<li><a href="http://p2.dpolly.me" title="Project 2"> Project 2 </a></li>
-				<li><a href="http://p3.dpolly.me" title="Project 3"> Project 3 </a></li>
-				<li><a href="http://p4.dpolly.me" title="Project 4"> Project 4 </a></li>
-			</ul>
+			<div class="text">
+				<h3>Project 1</H3>
+				<a href="git@github.com:dpolly/CSCIE15Project1.git" title="git@github.com:dpolly/CSCIE15Project1.git">GitHub URL</a>
+				<a href="http://p1.dpolly.me" title="http://p1.dpolly.me">Live URL </a> 
+				<h3>Project 2</H3>
+				<a href="git@github.com:dpolly/CSCIE15Project2.git" title="git@github.com:dpolly/CSCIE15Project2.git">GitHub URL</a>
+				<a href="http://p2.dpolly.me" title="http://p2.dpolly.me">Live URL </a> 
+				<h3>Project 3</H3>
+				<a href="git@github.com:dpolly/CSCIE15Project3.git" title="git@github.com:dpolly/CSCIE15Project3.git">GitHub URL</a>
+				<a href="http://p3.dpolly.me" title="http://p3.dpolly.me">Live URL</a>
+				<h3>Project 4</H3>
+				<a href="git@github.com:dpolly/CSCIE15Project4.git" title="git@github.com:dpolly/CSCIE15Project4.git">GitHub URL</a>
+				<a href="http://p4.dpolly.me" title="http://p4.dpolly.me">Live URL</a>
+			</div>
 		<h2 class="section"> Questions and Answers</h2>
 			<div class="text"> 
-				<p>Question: </br> akdjladkjfaldkjfaldkfjaldkjf;alkdfj;aldkjf;aldkjf;ladkfj;lakdjfla;dkjf;aldkjf</p>
-				<p>Answer: </br>sdfadlskjaldkfja;dkja;ldkfjaldksdfad sfadsfadfadfadfadfkadf;adkjfa;lkdjf;alkdsjf;alkdsjf;alkdsf j;lakdsjf;aldskfj;aldskfj;lasdkjfa;lkdsfja;lkdsfja;ldskfj;alsdkjf;lasdkfj;aldskfja;lsdkfja;ldskjfjf;alskdjf </p>
-				<p>Question: </br> akdjladkjfaldkjfaldkfjaldkjf;alkdfj;aldkjf;aldkjf;ladkfj;lakdjfla;dkjf;aldkjf</p>
-				<p>Answer: </br>sdfadlskjaldkfja;dkja;ldkfjaldksdfad sfadsfadfadfadfadfkadf;adkjfa;lkdjf;alkdsjf;alkdsjf;alkdsf j;lakdsjf;aldskfj;aldskfj;lasdkjfa;lkdsfja;lkdsfja;ldskfj;alsdkjf;lasdkfj;aldskfja;lsdkfja;ldskjfjf;alskdjf </p>
+				<p>Question: Who are you?</p>
+				<p>Answer: <br/></p>
+				<p>Question: What do you do?</p>
+				<p>Answer: <br/></p>
+				<p>Question: Why are you taking this course?</p>
+				<p>Answer: <br/></p>
+				<p>Question: What previous experience do you have with programming?</p>
+				<p>Answer: <br/></p>
+				<p>Question: Are you working on a Mac, PC, or some other platform?</p>
+				<p>Answer: <br/>PC</p>
 			</div>
 		<footer>
 			<?php include("includes/footer.php"); ?> 
